5ed82eaf7d4ba79e4ea6e3e5597c7a6cd58a75de
ssamansoni <83963885+ssamansoni@users.noreply.github.com>
2022-07-07 20:11:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+//jshint eversion: 6
+const express=require("express");
+const bodyparser=require("body-parser");
+const date=require(__dirname+"/date.js");
+const mongoose=require("mongoose");
+
+
+const app=express();
+mongoose.connect("mongodb://localhost:27017/todolistDB" , { useNewUrlParser : true});
+const itemsschema={
+  name:String
+};
+const Item=mongoose.model("Item",itemsschema);
+
+const buy= new Item({
+  name:"Hit the right checkbox to delete the task."
+});
+const sell= new Item({
+  name:"Sell food"
+});
+const eat= new Item({
+  name:"Eat food"
+});
+const defaultarray=[buy];
+app.set('view engine','ejs');
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+const day=date.getDate();
+
+
+const p=Item.find({},(err,founditems)=>{
+  if(founditems.length==0){
+    Item.insertMany(defaultarray,(err)=>{
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log("sucessfully added");
+      }
+    });
+    res.redirect("/");
+  }
+  else{
+
+      res.render("list",{acday : day,newListItems:founditems });
+
+  }
+});
+
+});
+
+app.post("/",(req,res)=>{
+  let item=req.body.newitem;
+  let tempitem=new Item({
+    name : item
+  });
+  tempitem.save();
+  res.redirect("/");
+})
+
+app.post("/delete",(req,res)=>{
+  let tempid=req.body.checkbox;
+  Item.findByIdAndRemove(tempid,(err)=>{
+    if(!err){
+      console.log("sucessfully deleted");
+    }
+  })
+  res.redirect("/");
+})
+app.listen(3000,()=>{
+  console.log("You popped into the server");
+})
---
+++ date.js
@@ -0,0 +1,10 @@
+
+module.exports.getDate=()=>{
+var today=new Date();
+var options={
+weekday : "long",
+day : "numeric",
+month: "long"
+};
+return today.toLocaleDateString("en-US",options);
+}
